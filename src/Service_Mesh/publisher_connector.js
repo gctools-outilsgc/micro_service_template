e8a5534ea1ebeae1aa6792d3d19898f867a383b3
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2019-07-18 16:22:39 -0400

    Added more robustness to AMQP connector

--- src/Service_Mesh/publisher_connector.js
+++ src/Service_Mesh/publisher_connector.js
@@ -5,27 +5,19 @@ const amqp = require("amqplib/callback_api");
 var publisherConnection = null;
 var publishChannel = null;
 
-async function publishMessageQueue(exchange, key, message){
+// configurable settings
+var exchange = "default";
+var retryDelay = 5000;
 
-    var retryDelay = 5000;
+async function publishMessageQueue(exchange, key, msgBody){
 
-    if (publishChannel == null){
-        publishChannel = await publisherConnection.createChannel();
-
-        publishChannel.on("error", function(err) {
-            // eslint-disable-next-line no-console
-            console.error("[SMQ] channel error", err.message);
-        });
-        publishChannel.on("close", function() {
-            // eslint-disable-next-line no-console
-            console.info("[SMQ] channel '" + exchange + "' closed");
-            setTimeout(function() {
-                publishMessageQueue(exchange, key); 
-            },retryDelay);
-        }); 
+    // If there is no Message Queue Connection then do not try to send a message
+    if(!publisherConnection){
+        return;
     }
+    
     try {
-        await publishChannel.publish(exchange, key, new Buffer(JSON.stringify(message), {persistent:true}));
+        await publishChannel.publish(exchange, key, Buffer.from(JSON.stringify(msgBody), {persistent:true}));
     } catch(err){
         // eslint-disable-next-line no-console
         console.error("[SMQ] error", err);
@@ -33,14 +25,33 @@ async function publishMessageQueue(exchange, key, message){
     }
 }
 
+function createChannel(connection){
+    publishChannel = connection.createChannel();
+    publishChannel.assertExchange(exchange, "topic", {
+        durable: true
+    });
+
+    publishChannel.on("error", function(err) {
+        // eslint-disable-next-line no-console
+        console.error("[SMQ] channel error", err.message);
+    });
+    publishChannel.on("close", function() {
+        // eslint-disable-next-line no-console
+        console.info("[SMQ] channel '" + exchange + "' closed");
+        setTimeout(function() {
+            createChannel(connection); 
+        },retryDelay);
+    }); 
+}
+
 // if the connection is closed or fails to be established at all, we will reconnect
 function connectMessageQueuePublisher(){
-  
-    amqp.connect("amqp://" + config.rabbitMQ.user+ ":" + config.rabbitMQ.password + "@" + config.rabbitMQ.host +"?heartbeat=60", function(err, conn) {
+
+    amqp.connect("amqp://" + config.rabbitMQ.user + ":" + config.rabbitMQ.password + "@" + config.rabbitMQ.host +"?heartbeat=60", function(err, conn) {
         if (err) {
             // eslint-disable-next-line no-console
             console.error("[SMQ]", err.message);
-            return setTimeout(connectMessageQueuePublisher, 5000);
+            return setTimeout(connectMessageQueuePublisher, retryDelay);
         }
         conn.on("error", function(err) {
             if (err.message !== "Connection closing") {
@@ -51,12 +62,14 @@ function connectMessageQueuePublisher(){
         conn.on("close", function() {
               // eslint-disable-next-line no-console
                 console.error("[SMQ] reconnecting");
-                return setTimeout(connectMessageQueuePublisher, 5000);
+                return setTimeout(connectMessageQueuePublisher, retryDelay);
         });
 
         // eslint-disable-next-line no-console      
         console.info("[SMQ] connected");
-        publisherConnection = conn;
+        publisherConnection = conn;      
+        
+        createChannel(publisherConnection);
     });
 }
 
